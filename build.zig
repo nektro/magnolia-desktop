5f5b9de672e71cfd0a15e708bd30fe15f57587e8
Meghan Denny <hello@nektro.net>
2022-05-07 21:48:56 -0700

    build.zig: only build all apps if in debug mode or if -Dall is passed

--- build.zig
+++ build.zig
@@ -2,10 +2,14 @@ const std = @import("std");
 const string = []const u8;
 const deps = @import("./deps.zig");
 
+var doall: bool = false;
+
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
 
     const mode = b.standardReleaseOptions();
+
+    doall = b.option(bool, "all", "Build all apps, default only selected steps") orelse (mode == .Debug);
 
     addExe(b, target, mode, "triangle", "apps/triangle.zig");
     addExe(b, target, mode, "demo-centersquare", "apps/demo-centersquare.zig");
@@ -13,6 +17,7 @@ pub fn build(b: *std.build.Builder) void {
     addExe(b, target, mode, "triangle-raw", "apps/triangle-raw.zig"); // temp for debugging
     addExe(b, target, mode, "demo-layout", "apps/demo-layout.zig");
     addExe(b, target, mode, "demo-layout2", "apps/demo/layout2.zig");
+    addExe(b, target, mode, "raw", "apps/raw.zig");
 }
 
 fn addExe(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.Mode, comptime name: string, root_src: string) void {
@@ -20,7 +25,7 @@ fn addExe(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.
     exe.setTarget(target);
     exe.setBuildMode(mode);
     deps.addAllTo(exe);
-    exe.install();
+    if (doall) exe.install();
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -30,4 +35,5 @@ fn addExe(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.
 
     const run_step = b.step(name, "Run the " ++ name ++ "app");
     run_step.dependOn(&run_cmd.step);
+    run_step.dependOn(&b.addInstallArtifact(exe).step);
 }

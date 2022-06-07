e77080a21ac3e4b0daa85a7c1d038548d70dab05
Meghan Denny <hello@nektro.net>
2022-06-06 19:24:56 -0700

    mag.TextLine: vertical alignment improvements

--- src/TextLine.zig
+++ src/TextLine.zig
@@ -19,18 +19,24 @@ pub fn draw(self: Self, app: root.App, x: u32, y: u32, width: u32, height: u32) 
     _ = width;
     _ = height;
 
+    const font = self.style.font.?;
+    const fh = font.properties.ints.get("CAP_HEIGHT").? + 1;
+
     var pts = std.ArrayList(mag.gl.Path.Item).init(app.alloc);
     defer pts.deinit();
     var iter = std.unicode.Utf8View.initUnchecked(self.text).iterator();
     var dx: u32 = 0;
     while (iter.nextCodepoint()) |cp| {
-        const char = self.style.font.?.getChar(@intCast(u16, cp));
+        const char = font.getChar(@intCast(u16, cp));
+        const cx = (char.ll_x - 1) * scale;
+        const cy = (char.ll_y + fh - char.height) * scale;
+
         for (range(char.height * scale)) |_, py| {
             for (range(char.width * scale)) |_, px| {
                 if (!char.bits[extras.d2index(char.height, py / scale, px / scale)]) continue;
                 try pts.append(mag.gl.vertexp(
-                    x + dx + @intCast(u32, px),
-                    y + @intCast(u32, py),
+                    add(x, cx) + dx + @intCast(u32, px),
+                    add(y, cy) + @intCast(u32, py),
                 ));
             }
         }
@@ -38,6 +44,13 @@ pub fn draw(self: Self, app: root.App, x: u32, y: u32, width: u32, height: u32) 
     }
     mag.gl.color3(0, 0, 0);
     mag.gl.drawPoints(pts.items);
+}
+
+fn add(a: u32, b: i32) u32 {
+    if (b >= 0) {
+        return a + @intCast(u31, b);
+    }
+    return a + @intCast(u31, -b);
 }
 
 pub fn getWidth(self: Self, app: root.App) u32 {

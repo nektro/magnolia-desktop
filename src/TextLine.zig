c5fcc27df7d3a301d2e33b7024ba7790c5101ada
Meghan Denny <hello@nektro.net>
2022-06-07 11:10:57 -0700

    mag.bdf: final alignment fixes

--- src/TextLine.zig
+++ src/TextLine.zig
@@ -17,6 +17,7 @@ pub fn draw(self: Self, app: root.App, x: u32, y: u32, width: u32, height: u32) 
 
     const font = self.style.font.?;
     const fh = font.properties.ints.get("CAP_HEIGHT").? + 1;
+    const min_width = @intCast(u8, font.properties.ints.get("FIGURE_WIDTH").?);
 
     var pts = std.ArrayList(mag.gl.Path.Item).init(app.alloc);
     defer pts.deinit();
@@ -25,7 +26,7 @@ pub fn draw(self: Self, app: root.App, x: u32, y: u32, width: u32, height: u32) 
     while (iter.nextCodepoint()) |cp| {
         const char = font.getChar(@intCast(u16, cp));
         const cx = (char.ll_x - 1) * scale;
-        const cy = (char.ll_y + fh - char.height) * scale;
+        const cy = (-char.ll_y + fh - char.height) * scale;
 
         for (range(char.height * scale)) |_, py| {
             for (range(char.width * scale)) |_, px| {
@@ -36,7 +37,7 @@ pub fn draw(self: Self, app: root.App, x: u32, y: u32, width: u32, height: u32) 
                 ));
             }
         }
-        dx += char.width * scale;
+        dx += std.math.max(char.width, min_width) * scale;
     }
     mag.gl.color3(0, 0, 0);
     mag.gl.drawPoints(pts.items);
@@ -46,16 +47,18 @@ fn add(a: u32, b: i32) u32 {
     if (b >= 0) {
         return a + @intCast(u31, b);
     }
-    return a + @intCast(u31, -b);
+    return a - @intCast(u31, -b);
 }
 
 pub fn getWidth(self: Self, app: root.App) u32 {
     _ = app;
+    const font = self.style.font.?;
+    const min_width = @intCast(u8, font.properties.ints.get("FIGURE_WIDTH").?);
     var res: u32 = 0;
     var iter = std.unicode.Utf8View.initUnchecked(self.text).iterator();
     while (iter.nextCodepoint()) |cp| {
         const char = self.style.font.?.getChar(@intCast(u16, cp));
-        res += char.width;
+        res += std.math.max(char.width, min_width);
     }
     return res * scale;
 }

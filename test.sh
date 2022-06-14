9b7a5457fdf3474e0ddfc13c06705de49b15ef80
Meghan Denny <hello@nektro.net>
2022-06-13 23:58:05 -0700

    meta: add test.sh that runs all apps to ensure visual quality

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+time zig build --prominent-compile-errors -Dall -Drelease-safe
+
+./zig-out/bin/magnolia-triangle
+./zig-out/bin/magnolia-demo-centersquare
+./zig-out/bin/magnolia-demo-focusblur
+./zig-out/bin/magnolia-demo-layout
+./zig-out/bin/magnolia-demo-layout2
+./zig-out/bin/magnolia-demo-margin
+./zig-out/bin/magnolia-demo-text
+./zig-out/bin/magnolia-demo-centersquare2
